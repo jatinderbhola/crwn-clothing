ed1b6bf7b648468e29a8fb3ff1a3095b8f365fe6
jatinder bhola <jatinderbhola@gmail.com>
2020-08-09 18:21:13 -0400

    install react-router-dom :) - jay

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
 import './App.css';
 import HomePage from './pages/homepage/homepage.components';
+
+
 
 function App() {
   return (
