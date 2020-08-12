73607c374cc40f822fc3ac66d4ff0239996bef42
jatinder bhola <jatinderbhola@gmail.com>
2020-08-12 08:23:18 -0400

    sign in component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Route, Switch, } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.components';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component';
+
 
 import './App.css';
 
@@ -14,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInSignUpPage} />
       </Switch>
     </div>
   );
