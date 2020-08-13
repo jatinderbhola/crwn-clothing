ad93d7620de0f6ccb1e94bcc3c3f879fe3a8b3a4
jatinder bhola <jatinderbhola@gmail.com>
2020-08-13 01:59:20 -0400

    added cart redux, action types, reducers, header cart icon with toggle, and design - jay

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,8 +5,10 @@ import "./header.styles.scss";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { Link } from "react-router-dom";
 import { auth } from "../../firebase/firebase.utils";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className="header">
         <Link className="logo-container" to="/">
             <Logo className="logo" />
@@ -27,13 +29,16 @@ const Header = ({ currentUser }) => (
                     SIGNIN
                 </Link>
             )}
+            <CartIcon />
         </div>
+        {hidden ? null : <CartDropdown />}
     </div>
 );
 
 // allowed to access the state
-const mapStateToProps = (state) => ({
-    currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden,
 });
 
 export default connect(mapStateToProps)(Header);
