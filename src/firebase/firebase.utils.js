287c0655083cc063d26e913e4d46f34e98174c77
jatinder bhola <jatinderbhola@gmail.com>
2020-08-12 22:41:19 -0400

    firebox config removed - jay

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,17 +1,9 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import { FIREBASE_CONFIG } from './config'
 
-const config = {
-    apiKey: "AIzaSyBNFizGCRFJCzvLRXP7xP3yGUrKx1YlH-I",
-    authDomain: "crwn-db-5ff60.firebaseapp.com",
-    databaseURL: "https://crwn-db-5ff60.firebaseio.com",
-    projectId: "crwn-db-5ff60",
-    storageBucket: "crwn-db-5ff60.appspot.com",
-    messagingSenderId: "850062146207",
-    appId: "1:850062146207:web:7837b51ff73801d66180aa",
-    measurementId: "G-BKSHXDR9BX"
-};
+const config = FIREBASE_CONFIG;
 
 firebase.initializeApp(config);
 
