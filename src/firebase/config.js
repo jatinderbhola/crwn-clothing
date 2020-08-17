0bc897191fa7a1c2e145bdd7f6abc82839d5295d
jatinder bhola <jatinderbhola@gmail.com>
2020-08-17 13:15:25 -0400

    add config - jay

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyBNFizGCRFJCzvLRXP7xP3yGUrKx1YlH-I",
+    authDomain: "crwn-db-5ff60.firebaseapp.com",
+    databaseURL: "https://crwn-db-5ff60.firebaseio.com",
+    projectId: "crwn-db-5ff60",
+    storageBucket: "crwn-db-5ff60.appspot.com",
+    messagingSenderId: "850062146207",
+    appId: "1:850062146207:web:7837b51ff73801d66180aa",
+    measurementId: "G-BKSHXDR9BX"
+};;
