b7eb977ae17252736a662ea5010945efd02e2704
jatinder bhola <jatinderbhola@gmail.com>
2020-08-20 21:52:20 -0400

    cart, cart util, add to cart, quantity, cart reducer - jay

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import "./cart-dropdown.styles.scss";
 import CustomButton from "../custom-button/custom-button.component";
+import { connect } from "react-redux";
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => (
     <div className="cart-dropdown">
         <div className="cart-item"></div>
+
+        {cartItems.map((item) => (
+            <div key={item.id}>
+                <p> {item.name}</p>
+                <p> {item.imageUrl}</p>
+                {/* <p> {item.quantity}</p> */}
+            </div>
+        ))}
+
         <CustomButton> Go To Checkout</CustomButton>
     </div>
 );
 
-export default CartDropdown;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    cartItems,
+});
+
+export default connect(mapStateToProps)(CartDropdown);
