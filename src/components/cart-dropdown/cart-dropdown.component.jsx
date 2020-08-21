85407a3ef191f1c0a11e5b26d1da885ecd4e7582
jatinder bhola <jatinderbhola@gmail.com>
2020-08-20 22:06:12 -0400

    cart item componentm and connectioe with cart dropdown and reducer - jay

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,19 +2,13 @@ import React from "react";
 import "./cart-dropdown.styles.scss";
 import CustomButton from "../custom-button/custom-button.component";
 import { connect } from "react-redux";
+import CartItem from "../cart-item/cart-item.component";
 
 const CartDropdown = ({ cartItems }) => (
     <div className="cart-dropdown">
-        <div className="cart-item"></div>
-
         {cartItems.map((item) => (
-            <div key={item.id}>
-                <p> {item.name}</p>
-                <p> {item.imageUrl}</p>
-                <p> {item.quantity}</p>
-            </div>
+            <CartItem key={item.id} item={item} />
         ))}
-
         <CustomButton> Go To Checkout</CustomButton>
     </div>
 );
