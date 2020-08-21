e9604e3c7b98ef0a62fbd0658586ef3fa0dba645
jatinder bhola <jatinderbhola@gmail.com>
2020-08-20 22:34:48 -0400

    updating application with reselect, replace cart selector with memorize - jay

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,19 +3,24 @@ import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import "./cart-icon.styles.scss";
 import { connect } from "react-redux";
 import { toggleCartHidden } from "../../redux/cart/cart.action";
+import { selectCartItemCount } from "../../redux/cart/cart.selector";
 
-const CartIcon = ({ toggleCartHidden, cartItems }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
     <div className="cart-icon" onClick={toggleCartHidden}>
         <ShoppingIcon className="shopping-icon" />
-        <span className="item-count">{cartItems.length}</span>
+        <span className="item-count">{itemCount}</span>
     </div>
 );
 
 const mapDispatchToProps = (dispatch) => ({
     toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
+// NOTEME: SELECTOR
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-    cartItems,
+// ISSUE: render component every time
+// Solution: Memorization
+// lib: re select
+const mapStateToProps = (state) => ({
+    itemCount: selectCartItemCount(state),
 });
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
